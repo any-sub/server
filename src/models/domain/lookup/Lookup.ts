328f04eaf289c7c01f82b5ca570df2cfbc24c355
Stefan <stefan@stefanpuia.co.uk>
2023-02-26 22:23:49 +0000

    removed old lookup files

--- src/models/domain/lookup/Lookup.ts
+++
@@ -1,9 +0,0 @@
-export abstract class Lookup {
-  public enabled: boolean;
-  public name: string;
-  public frequency: number;
-}
-
-export enum SelectorType {
-  CSS = "CSS"
-}
--- src/models/domain/lookup/SingleHtmlElementLookup.ts
+++
@@ -1,9 +0,0 @@
-import { Lookup, SelectorType } from "./Lookup";
-
-export class SingleHtmlElementLookup extends Lookup {
-  public id: string;
-  public url: string;
-  public selector: string;
-  public selectorType: SelectorType;
-  public expression?: RegExp;
-}
