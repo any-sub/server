1facef7bb12e5ec711f07f8223adbb4de8d42d44
Stefan <stefan@stefanpuia.co.uk>
2023-05-11 20:32:57 +0100

    fixed test

--- src/controllers/login/LoginController.integration.spec.ts
+++ src/controllers/login/LoginController.integration.spec.ts
@@ -1,30 +1,25 @@
-import { PlatformTest } from "@tsed/common";
+import {PlatformTest} from "@tsed/common";
 import SuperTest from "supertest";
-import { LoginController } from "./LoginController";
-import { Server } from "../../Server";
+import {LoginController} from "./LoginController";
+import {Server} from "../../Server";
 
 describe("LoginController", () => {
   let request: SuperTest.SuperTest<SuperTest.Test>;
 
-  beforeEach(
-    PlatformTest.bootstrap(Server, {
-      mount: {
-        "/": [LoginController]
-      }
-    })
-  );
-  beforeEach(() => {
+  beforeAll(PlatformTest.bootstrap(Server));
+  beforeAll(() => {
     request = SuperTest(PlatformTest.callback());
   });
+  afterAll(PlatformTest.reset);
 
-  afterEach(PlatformTest.reset);
+  describe("GET /login", () => {
+    it("should return login page", async () => {
+      const response = await request.get("/login").expect(200);
 
-  it("should call GET /login", async () => {
-    const response = await request.get("/login").expect(200);
-
-    expect(response.type).toEqual("text/html");
-    expect(response.text).toContain("Login");
-    expect(response.text).toContain("Email");
-    expect(response.text).toContain("Password");
-  });
+      expect(response.type).toEqual("text/html");
+      expect(response.text).toContain("Login");
+      expect(response.text).toContain("Email");
+      expect(response.text).toContain("Password");
+    });
+  })
 });
