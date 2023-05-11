1343add6b2db4813adbbfdabd72577186a757d60
Stefan <stefan@stefanpuia.co.uk>
2023-05-11 22:09:39 +0100

    added worker-transport

--- README.md
+++ README.md
@@ -8,9 +8,13 @@ Based on the [Ts.ED](https://tsed.io) framework.
 
 * Node v16+
 * Yarn
+* [Google Cloud CLI](https://cloud.google.com/sdk/docs/install-sdk)
 
 
 ```bash
+# authenticate
+yarn auth
+
 # install dependencies
 yarn
 
@@ -25,4 +29,4 @@ yarn test
 ```
 
 > Creating or deleting some files may require barrelsby to recreate the index files.
-> Starting the worker automatically runs barrelsby.
+> Starting the server automatically runs barrelsby.
--- src/service/jobs/JobService.ts
+++ src/service/jobs/JobService.ts
@@ -2,6 +2,7 @@ import { Agenda, Every } from "@tsed/agenda";
 import { Inject } from "@tsed/di";
 import { WorkerManager } from "../../components";
 import { Logger } from "@tsed/logger";
+import { WorkParser } from "@any-sub/worker-transport";
 
 @Agenda({ namespace: "schedule" })
 export class JobService {
@@ -11,9 +12,9 @@ export class JobService {
   @Every("5 seconds", { name: "work" })
   async scheduleWork() {
     try {
-      this.workerManager.requestWork(new Date());
+      this.workerManager.requestWork(WorkParser.parse({ id: "4ea57865-ef03-4bac-94a0-8fb93fcf5bff", type: "http" }));
     } catch (err) {
-      this.logger.info(err);
+      this.logger.debug(err);
     }
   }
 }
